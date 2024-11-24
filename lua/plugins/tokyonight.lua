47517c5244420059887d3161aa4700074863883e
Andrey Svetlichny <andrey.svetlichny@gmail.com>
2024-11-24 18:17:05 +0100

    tokyonight tuning

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -3,7 +3,23 @@ return {
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
+    require("tokyonight").setup({
+      dim_inactive = false,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+      },
+      on_colors = function(colors)
+        -- tune colors
+        -- :hi Comment guifg=#FFFF00
+        -- :hi Normal guibg=#16161e
+        colors.comment = "#7885bf"
+        colors.bg = "#16161e"
+        colors.bg_dark = "#000000"
+      end
+    })
+
     -- load the colorscheme here
-    vim.cmd([[colorscheme tokyonight]])
+    vim.cmd([[colorscheme tokyonight-night]])
   end,
 }
