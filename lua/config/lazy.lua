9dec059a605ffc726c9bf3e3ea83e9af25cb79be
Andrey Svetlichny <andrey.svetlichny@gmail.com>
2024-11-28 12:16:50 +0100

    keymap C-q

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,6 +14,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require('config.keymaps')
 require('config.options')
 
 require("lazy").setup({
@@ -21,5 +22,5 @@ require("lazy").setup({
     { import = "plugins" },
   },
   install = { colorscheme = { "habamax" } },
-  checker = { enabled = true },
+--  checker = { enabled = true },
 })
